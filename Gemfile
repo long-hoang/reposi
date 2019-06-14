8e303c433f612d82606b7e620029982e4755aafc
long-hoang <thoanglong@gmail.com>
2019-06-14 03:54:56 +0000

    add simple_form

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'uglifier', '>= 1.3.0'
 # gem 'mini_racer', platforms: :ruby
 
 
-
+gem 'simple_form'
 
 
 gem 'jquery-rails'
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,4 +2,9 @@ class QuotesController < ApplicationController
   def index
     @quote = Quote.order("RANDOM()").first
   end
+
+  def new 
+ 
+  end
+
 end
